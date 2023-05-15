b005d92a6a477e443eb7eb5b2f89c27c2ef06ee3
vlad <kuznetsov.vp@phystech.edu>
2023-05-16 02:38:51 +0300

    add gallery_script.js

---
+++ scripts/gallery_script.js
@@ -0,0 +1,63 @@
+const popup = document.getElementById('.popup');
+const popupImage = document.getElementById('.popup__image');
+const closeButton = document.querySelector('.popup__exit-button');
+const prevButton = document.querySelector('.popup__arrow_left');
+const nextButton = document.querySelector('.popup__arrow_right');
+const galleryImages = document.querySelector('.rectangle__image');
+
+let currentIndex = 0;
+let imageSrc;
+
+function showPopup(index) {
+    imageSrc = galleryImages[index].querySelector('img').src;
+    popupImage.src = imageSrc;
+    popup.style.display = 'flex';
+    currentIndex = index;
+
+    updateNavigationButtons();
+}
+
+function updateNavigationButtons() {
+    if (currentIndex === 0) {
+        prevButton.style.display = 'none';
+    } else {
+        prevButton.style.display = 'flex';
+    }
+
+    if (currentIndex === galleryItems.length - 1) {
+        prevButton.style.display = 'none';
+    } else {
+        prevButton.style.display = 'flex';
+    }
+}
+
+function showPreviousImage() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        imageSrc = galleryImages[currentIndex].querySelector('img').src;
+        popupImage.src = imageSrc;
+        updateNavigationButtons();
+    }
+}
+
+function showNextImage() {
+    if (currentIndex > 0) {
+        currentIndex++;
+        imageSrc = galleryImages[currentIndex].querySelector('img').src;
+        popupImage.src = imageSrc;
+        updateNavigationButtons();
+    }
+}
+
+for (let i = 0; i < galleryItems.length; i++) {
+    galleryImages[i].addEventListener('click', function() {
+        showPopup(i);
+    });
+}
+
+closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+prevButton.addEventListener('click', showPreviousImage);
+nextButton.addEventListener('click', showNextImage);
